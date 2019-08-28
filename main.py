ad99251140966e6771835033f0ae78525156a1b4
Ryan <ryan.shi@live.com>
2019-08-29 07:13:40 +0800

    03

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 #-*- coding: utf-8 -*-
-from django_micro import configure, route, run
-from django.http import HttpResponse
+# from django_micro import configure, route, run
+# from django.http import HttpResponse
+#
+# DEBUG = True
+# limit = 10
+#
+# configure(locals())
+#
+# @route('', name='home')
+# def homepage(request):
+#     global limit
+#     limit = limit - 1
+#     limit2 = 10-limit
+#     html = f'''
+#     <div style="background-color:#77DFF5;height:100%">
+# 		<h1 style="color:white;text-align:center">It's your {limit2  } times visit~ </h1>
+#         <h1 style="color:white;text-align:center">You have { limit } times left~ </h1>
+#         <image src="https://file-geieweypjd.now.sh/" style="margin:auto;display:block">
+#     </div>
+#     '''
+#     print(limit)
+#     if limit > 0:
+#         return HttpResponse(html)
+#     else:
+#         return HttpResponse("haha can't touch this")
+#
+# application = run()
 
-DEBUG = True
-limit = 10
-
-configure(locals())
-
-@route('', name='home')
-def homepage(request):
-    global limit
-    limit = limit - 1
-    limit2 = 10-limit
-    html = f'''
-    <div style="background-color:#77DFF5;height:100%">
-		<h1 style="color:white;text-align:center">It's your {limit2  } times visit~ </h1>
-        <h1 style="color:white;text-align:center">You have { limit } times left~ </h1>
-        <image src="https://file-geieweypjd.now.sh/" style="margin:auto;display:block">
-    </div>
-    '''
-    print(limit)
-    if limit > 0:
-        return HttpResponse(html)
-    else:
-        return HttpResponse("haha can't touch this")
-
-application = run()
+user_input = ""
+# another_thing = False
+if user_input:
+    print("hello")
+elif user_input.startswith("1"):
+    print("we got 1 ")
+elif user_input.endswith("2"):
+    print("we got 2")
+# else:
+#     print("nothing")
