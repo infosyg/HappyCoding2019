74c738ded6848ed53d9602a94c2c366ea46e77ab
Ryan <ryan.shi@live.com>
2019-09-12 06:41:44 +0800

    u

--- p2/PycharmProjects/uploadfile-2/index.py
+++ p2/PycharmProjects/uploadfile-2/index.py
@@ -1,11 +1,16 @@
-"https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
-"https://extra-uru1z3cxu.now.sh/css/extra.css"
-"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css"
-'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'
+# "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+# "https://extra-uru1z3cxu.now.sh/css/extra.css"
+# "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css"
+# 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'
 
 import dominate.tags as dom 
 from dominate.document import document
+from django_micro import configure,route,run
+from django.http import HttpRequest,HttpResponse
+CENTER_FRAME = "flex flex-col bg-white shadow-xl p-3 rounded-lg w-120 h-120"
+upload_form = "flex fle"
 
+configure({"DEBUG":True})
 def link(lk):
     return dom.link(rel="stylesheet",type="text/css",href=lk)
 
@@ -16,10 +21,7 @@ def page():
         link_("https://extra-uru1z3cxu.now.sh/css/extra.css")
         link_("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css")
         link_('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js')
-with doc.body:
-
-
+    with doc.body:
+        pass
     return doc.render()
-
-
 print(page())
