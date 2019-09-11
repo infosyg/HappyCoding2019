dd12206c57dda31ecc834f50810f53818de23e49
Ryan <ryan.shi@live.com>
2019-09-11 21:07:59 +0800

    hel

---
+++ p2/verysimple/urls.py
@@ -0,0 +1,11 @@
+"""URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+"""
+from django.urls import path
+from views import index
+
+urlpatterns = [
+    path('', index),
+]
