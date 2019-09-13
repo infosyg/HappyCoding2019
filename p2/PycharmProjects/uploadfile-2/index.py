1b741dd5a1e0c004114b6acaec79c34afa5c83e6
ryans <ryan.shi@live.com>
2019-09-13 22:19:26 +0800

    03

--- p2/PycharmProjects/uploadfile-2/index.py
+++ p2/PycharmProjects/uploadfile-2/index.py
@@ -17,10 +17,10 @@ def link(lk):
 def page():
     doc = document()
     with doc.head:
-        link_("https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css")
-        link_("https://extra-uru1z3cxu.now.sh/css/extra.css")
-        link_("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css")
-        link_('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js')
+        link("https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css")
+        link("https://extra-uru1z3cxu.now.sh/css/extra.css")
+        link("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css")
+        link('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js')
     with doc.body:
         pass
     return doc.render()
